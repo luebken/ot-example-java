915c6938a91ab6e13b5742236276496eebe46774
Matthias Luebken <forname dot lastname at gmail dot com>
2018-02-21 14:46:13 +0100

    mark as error and log a message

--- product/src/main/java/com/example/product/ProductController.java
+++ product/src/main/java/com/example/product/ProductController.java
@@ -1,6 +1,6 @@
 package com.example.product;
 
-import java.util.Map;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.opentracing.Span;
 import io.opentracing.SpanContext;
+import io.opentracing.log.Fields;
 import io.opentracing.propagation.Format;
 import io.opentracing.propagation.TextMapExtractAdapter;
+import io.opentracing.tag.Tags;
 
 @RestController
 public class ProductController {
@@ -24,10 +26,17 @@ public class ProductController {
 
         SpanContext parentSpan = tracer.extract(Format.Builtin.HTTP_HEADERS, new TextMapExtractAdapter(headers));
 
+        // Create a child span as an error.
         Span span = tracer.buildSpan("/product").asChildOf(parentSpan).start();
-
-        // doing some work
-        // Dummy work
+        span.setTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_SERVER);
+        span.setTag(Tags.ERROR.getKey(), true);
+        
+        // Additionally log that error.
+        Map<String, String> logs = new HashMap<>();
+        logs.put(Fields.EVENT, "error");
+        logs.put("message", "this is bad");
+        span.log(logs);
+        // Doing some dummy work
         try {
             Thread.sleep(50);
         } catch (InterruptedException e) {
